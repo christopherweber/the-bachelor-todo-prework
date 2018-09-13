8937d4e7626612f4be9fea032e37946e7dc4efd7
Christopher Weber <chrisrrweber@gmail.com>
2018-09-13 16:06:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,14 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  full_name = ""
+  x=0
+  until full_name != ""
+    if data[season][x]["status"] == "Winner"
+      full_name = data[season][x]["name"]
+    else
+      x+= 1
+    end
+  end
+  return_string = full_name.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
