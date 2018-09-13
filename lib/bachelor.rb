afad5a0f2f765e840ee32facccea3ba50af18054
Christopher Weber <chrisrrweber@gmail.com>
2018-09-13 16:08:50 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+    return_num = 0
   data.each do |season,array|
     array.each do |contestant|
       if contestant["hometown"] == hometown
